b9746f3fd745c4918d34a9eec2762b732860b5d0
yungdenzel <oyomafia@gmail.com>
2019-11-28 18:31:51 +0100

    footer

--- src/components/Works.js
+++ src/components/Works.js
@@ -10,19 +10,19 @@ const Works = () => (
   <div>
     <div className="justify-center" style={{ backgroundColor: "#f9f9f9" }}>
       <div className="flex-l list justify-center helvetica pa5">
-        <div className="pa3 f3">
+        <div className="pa3 pr5 f3">
           <img className="justify-center " src={search} alt="search" />
           <h5 className="tc"> Search the Menu </h5>
         </div>
-        <div className="pa3 f3">
+        <div className="pa3 pr5 f3">
           <img className="justify-center " src={food} alt="food" />
           <h5 className="tc"> Order </h5>
         </div>
-        <div className="pa3 f3">
+        <div className="pa3  pr5 f3">
           <img className="justify-center" src={order} alt="order" />
-          <h5 className="tc"> Place your add </h5>
+          <h5 className="tc"> Place your address </h5>
         </div>
-        <div className="pa3 f3">
+        <div className="pa3 pr5 f3">
           <img className="justify-center " src={delivery} alt="delivery" />
           <h5 className="tc"> Get delivered </h5>
         </div>
