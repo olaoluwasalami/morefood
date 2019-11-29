0374fbe8ae27adc36530e6e11150a2668dbc88ab
yungdenzel <oyomafia@gmail.com>
2019-11-29 19:34:52 +0100

    carousel background

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import './Welcome.css';
 import { Container} from "semantic-ui-react";
+import BackgroundSlider from "react-background-slider";
 /* global google */
 
 
@@ -15,7 +15,8 @@ class Welcome extends Component {
   }
   componentDidMount(){
         this.autocomplete = new google.maps.places.Autocomplete(this.autocompleteInput.current, {
-               "types": ["geocode"]
+               types: ['establishment'],
+      componentRestrictions: {country: "ng"}
         });
 
         this.autocomplete.addListener('place_changed', this.handlePlaceChanged);
@@ -29,19 +30,28 @@ class Welcome extends Component {
 
   render() {
     return (
-      <div className="style">
+      <div>
+             <BackgroundSlider
+             images = {
+                 ['https://res.cloudinary.com/yungdenzel/image/upload/v1574581950/eiliv-sonas-aceron-ZuIDLSz3XLg-unsplash_p6a6e0.jpg',
+                 'https://res.cloudinary.com/yungdenzel/image/upload/v1575050845/amala_xt6q2m.jpg',
+                 'https://res.cloudinary.com/yungdenzel/image/upload/v1575051452/rice-n-jerk-2_oer4wh.jpg'
+            ]}
+       duration={6} transition={0} />
+                          
        <Container text>
           <h1 className="tc b white pb2 pt7">
             {" "}
             What would you like to order today ?
           </h1>
-          <form className="pb6">
+          <form className="pb6 flex">
             <input ref={this.autocompleteInput} id="autocomplete" placeholder="Enter your delivery address" type="text"
-              className="form-control fl"    
+              className = "form-control fl"
             />
             <input  type="submit" value="Find Food" className="fl ml2 bg-green white-90"/>
           </form>
         </Container>
+       
       </div>
     );
   }
